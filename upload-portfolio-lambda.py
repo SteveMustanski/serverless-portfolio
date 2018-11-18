22c2ad47593c0ffeb84455c188250205ca426e32
Steve Mustanski <steve.mustanski@gmail.com>
2018-11-18 11:53:59 -0500

    reworked first step python script to take zip and extract files to s3

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,53 +1,20 @@
 import boto3
-from botocore.client import Config
+
 import StringIO
 import zipfile
 import mimetypes
 
-def lambda_handler(event, context):
-    
-    sns = boto3.resource('sns');
-    topic = sns.Topic('arn:aws:sns:us-east-1:854423450523:deployPortfolioTopic')
+s3 = boto3.resource('s3')
 
-    location = {
-        "bucketName": "portfoliobuild.stevemustanski.com",
-        "objectKey": "portfoliobuild.zip"
-    }
+portfolio_bucket = s3.Bucket('portfolio.stevemustanski.com')
+build_bucket = s3.Bucket('portfoliobuild.stevemustanski.com')
 
-    try:
-        job = event.get('CodePipeline.job')
-        
-        if job:
-            for artifact in job["data"]["inputArtifacts"]:
-                if artifact["name"] == "MyAppBuild":
-                    location = artifact["location"]["s3location"]
-                    
-        print "Building portfolio from " + str(location)
-        
-        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
-        
-        
-        portfolio_bucket = s3.Bucket('portfolio.stevemustanski.com')
-        build_bucket = s3.Bucket(location['bucketName'])
-        
-        portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj(location['objectKey'], portfolio_zip)
-        
-        
-        with zipfile.ZipFile(portfolio_zip) as myzip:
-            for nm in myzip.namelist():
-                obj = myzip.open(nm)
-                portfolio_bucket.upload_fileobj(obj, nm,
-                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-        topic.publish(Subject="Portfolio Deployment Success", Message="There has been a deployment to the portfolio")
-        
-        if job:
-            codepipeline = boto3.client("codepipeline")
-            codepipeline.put_job_success_result(jobId=job["id"])
-        
-    except:
-        topic.publish(Subject="Portfolio Deployment Failed", Message="There has been a failed deployment to the portfolio")
-        raise
-            
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj, nm,
+          ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
